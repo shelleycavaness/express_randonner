d928c3d68935e41fef599b29c832f48618eecc94
shelley cavaness <shelleycavaness@yahoo.com>
2021-03-24 18:27:21 +0100

    deleted swagger

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-// var swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-// var swaggerDocument = require('swagger.json');
 
 
 router.use('/api', require('./api'));
-// router.use('/api-docs', swaggerUi.serve);
-// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 module.exports = router;
